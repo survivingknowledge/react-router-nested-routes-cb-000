2f862627a734dfae3c83dd0a6148a6545db40c11
Luke Ghenco <lukeghenco@gmail.com>
2017-02-15 07:46:53 -0500

    add starter code to MoviesNew component

---
+++ src/containers/MoviesNew.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addMovie } from '../actions';
+import { bindActionCreators } from 'redux';
+import { browserHistory } from 'react-router';
+
+class MoviesNew extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      title: ''
+    };
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.addMovie(this.state);
+    browserHistory.push('/movies');
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      title: event.target.value
+    });
+  }
+
+  render(){
+    return (
+      <form onSubmit={(event) => this.handleOnSubmit(event)} >
+        <input 
+          type="text" 
+          onChange={(event) => this.handleOnChange(event)} 
+          placeholder="Add a Movie" />
+        <input type="submit" value="Add Movie" />
+      </form>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addMovie: bindActionCreators(addMovie, dispatch)
+  };
+}
+
+export default connect(null, mapDispatchToProps)(MoviesNew)
